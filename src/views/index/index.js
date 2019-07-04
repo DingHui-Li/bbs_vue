21a6915e11532dcfa1c9c1403d3f3da3822b6d97
ldh <lidinghhui@outlook.com>
2019-07-04 23:38:20 +0800

    build web project

---
+++ src/views/index/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Index from './index.vue';
+new Vue({
+	render:h=>h(Index)
+}).$mount('#index')
---
+++ src/views/index/index.vue
@@ -0,0 +1,8 @@
+<template>
+	<div>11111111111111</div>
+</template>
+<script>
+export default {
+	
+}
+</script>
